13831ba57d7f3aa03f18b87ba2be701ef0bd6708
Denis <denismuchkaev@yandex.ru>
2021-12-27 14:30:49 +0300

    Updating stripe integration and updating our favicon and title

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import {selectShopCollections} from "../../redux/shop/shop.selectors";
+import {selecCollectionsForPreview} from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../collection-preview/collection-preview";
 
 import "./collections-overview.styles.scss";
@@ -15,7 +15,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+    collections: selecCollectionsForPreview
   })
   
 
